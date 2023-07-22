59a10223cdb8f74e715e91bec76edda1836a4fa3
jkapa7 <juandaniel5005@gmail.com>
2023-07-22 13:34:00 -0500

    Lenguages adjusts, now it can refresh page and still have the prefered lenguage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import "./App.css";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const [t, i18n] = useTranslation("global");
+
   return (
     <>
-      <h1>Multiple Lenguges!</h1>
+      <h1>{t("header.hello-word")}</h1>
+
+      <button onClick={() => i18n.changeLanguage("es")}>ES</button>
+      <button onClick={() => i18n.changeLanguage("en")}>EN</button>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,36 @@ import App from "./App.jsx";
 import "./index.css";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
+import global_es from "./translations/es/global.json";
+import global_en from "./translations/en/global.json";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-i18next.init({
-  interpolation: { escapeValue: false },
+i18next.use(LanguageDetector).init({
+  interpolation: {
+    escapeValue: false,
+  },
+
+  fallbackLng: "en",
+
+  debug: true,
+
+  react: {
+    wait: true,
+  },
+
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <I18nextProvider>
+    <I18nextProvider i18n={i18next}>
       <App />
     </I18nextProvider>
   </React.StrictMode>
